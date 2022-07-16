728734534ab239373b283b7c827c56dd8202907e
jaogrs <jgabrielrodrigues2@gmail.com>
2022-07-15 22:17:10 -0300

    feat: compras service

--- src/models/investimentos.model.js
+++ src/models/investimentos.model.js
@@ -1,8 +1,11 @@
 const connection = require('../db/investimentManager');
 
 const addInvestimento = (codCliente, codAtivo, qtdeAtivo, valor) => connection.execute(
-  'INSERT INTO InvestimentManager.investimento (CodCliente, CodAtivo, QtdeAtivo, Valor) VALUES(?, ?, ?, ?)',
+  'INSERT INTO InvestimentManager.investimentos (CodCliente, CodAtivo, QtdeAtivo, Valor) VALUES(?, ?, ?, ?)',
   [codCliente, codAtivo, qtdeAtivo, valor],
 );
 
-module.exports = { addInvestimento };
+const update = (codCliente, codAtivo, qtdeAtivo, valor) => connection.execute(`UPDATE InvestimentManager.investimentos SET 
+QtdeAtivo = ?, Valor = ? WHERE CodCliente = ? AND CodAtivo = ?`, [qtdeAtivo, valor, codCliente, codAtivo]);
+
+module.exports = { addInvestimento, update };
---
+++ src/services/compras.service.js
@@ -0,0 +1,9 @@
+const ativosModel = require('../models/ativosDisponiveis.model');
+const comprasModel = require('../models/compras.model');
+const investimentosModel = require('../models/investimentos.model');
+
+const addCompra = async () => {
+
+};
+
+module.exports = { addCompra };
