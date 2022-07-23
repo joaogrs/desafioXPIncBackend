84e8686f0710caa0e3dfdbb43c35aa4e9ea201ea
jaogrs <jgabrielrodrigues2@gmail.com>
2022-07-23 13:05:24 -0300

    fix: correção para o token funcionar no Swagger 2

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,7 +2,7 @@ const { authenticateToken } = require('../helpers/jwt');
 
 const authMiddleware = async (req, res, next) => {
   let token = req.headers.authorization;
-  if (token.includes('Bearer')) token = token.slice(7, token.length);
+  if (token && token.includes('Bearer')) token = token.slice(7, token.length);
 
   try {
     const payload = await authenticateToken(token);
